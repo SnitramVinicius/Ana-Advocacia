b7b705e639d33f28b8da0aee9b4ae7519170def7
Vinicius Martins <costa.vinicius2010@gmail.com>
2025-05-15 17:14:44 -0400

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Image from 'next/image'
 
 export default function Home() {
   return (
